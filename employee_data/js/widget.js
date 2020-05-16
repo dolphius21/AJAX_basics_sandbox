77065645e22c473fdd899cd7861921debd264d60
dolphius21 <johnrudolphlee@gmail.com>
2020-05-16 19:00:21 +0800

    added room widget

--- employee_data/js/widget.js
+++ employee_data/js/widget.js
@@ -1,8 +1,9 @@
-const xhr = new XMLHttpRequest();
+// Employees Widget
+const employeeWidget = new XMLHttpRequest();
 // Callback function
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    let employees = JSON.parse(xhr.responseText);
+employeeWidget.onreadystatechange = function () {
+  if (employeeWidget.readyState === 4) {
+    let employees = JSON.parse(employeeWidget.responseText);
     let statusHTML = '<ul class = "bulleted">';
     for (let i = 0; i < employees.length; i++) {
       if (employees[i].inoffice === true) {
@@ -18,6 +19,32 @@ xhr.onreadystatechange = function () {
   }
 };
 // Open request
-xhr.open('GET', 'data/employees.json');
+employeeWidget.open('GET', 'data/employees.json');
 // Send request
-xhr.send();
+employeeWidget.send();
+
+
+// Room Widget
+const roomWidget = new XMLHttpRequest();
+// Callback function
+roomWidget.onreadystatechange = function () {
+  if (roomWidget.readyState === 4) {
+    let rooms = JSON.parse(roomWidget.responseText);
+    let statusHTML = '<ul class = "bulleted">';
+    for (let i = 0; i < rooms.length; i++) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+// Open request
+roomWidget.open('GET', 'data/rooms.json');
+// Send request
+roomWidget.send();
